a15e50c2065cee095ce938eb4cd23748e453ead1
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-13 18:34:02 -0300

    feature(ADD user): Route plus controller Store user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'hello world braulio' });
-});
+/**
+ * Route: /users
+ * Route responsible to create a User in DB
+ */
+routes.post('/users', UserController.store);
 
 export default routes;
