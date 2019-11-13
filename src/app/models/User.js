4deff04116e298a9e55b18f52cee3649b9d05da8
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-13 19:01:27 -0300

    feature(password_hash): create a hash of password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,16 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
+/**
+ * This class represent the user model
+ */
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +18,18 @@ class User extends Model {
         sequelize,
       }
     );
+
+    /**
+     * Param: user
+     * Make the password's hash
+     */
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
