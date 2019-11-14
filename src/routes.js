febea65fe49bd7ab41fa531840d3571fec031a65
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-13 23:17:35 -0300

    feature(middleware checkSession): middleware global. Verify if the user have autorization

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import User from './app/models/User';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 /**
@@ -17,5 +19,16 @@ routes.post('/users', UserController.store);
  * Route responsible to create a session
  */
 routes.post('/sessions', SessionController.store);
+
+/**
+ * Global Middleware - Verify if user have a token
+ */
+routes.use(authMiddleware);
+
+/**
+ * Route: /users
+ * Route responsible to update a User in DB
+ */
+routes.put('/users', UserController.update);
 
 export default routes;
