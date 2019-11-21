bf2edd77dec6b397265649657ddae394ef46db02
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-21 18:27:06 -0300

    Chore(ENV_VARS): add .env file and configure

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  secure: false,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: process.env.MAIL_SECURE,
   auth: {
-    user: 'c0c5ce807f7270',
-    pass: '2eb176dd757e8f',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://76afde5e3de84a4ab125647f47725668@sentry.io/1829777',
+  dsn: process.env.SENTRY_DSN,
 };
