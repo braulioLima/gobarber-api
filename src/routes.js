56ff1209d550d743e0d400ae95ef66a05dccac34
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-21 13:10:02 -0300

    feature(available hour): check if provider have available hours

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -41,6 +42,11 @@ routes.put('/users', UserController.update);
  * Return all provider's
  */
 routes.get('/providers', ProviderController.index);
+
+/**
+ * Check available provider's hours
+ */
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 /**
  * Return all appointments
